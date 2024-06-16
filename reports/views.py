b99fd54607f2282ec005e6a81eb7a5430c6a96bd
Przemek <wisniewskip90@gmail.com>
2024-06-16 19:11:05 +0200

    add register_view in views.py

--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+from .forms import RegisterForm
 
 # Create your views here.
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
