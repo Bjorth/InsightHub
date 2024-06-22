6496d1dfdbcb52a07b2c7d197774e4ec34a71147
Przemek <wisniewskip90@gmail.com>
2024-06-22 13:52:47 +0200

    add merged navbar for views/create

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '0%r4y)4!&ea=!#av^%app-_^90bli7b5#cdr!70hpqyn&+i3=e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
