c2b1245b54b50fe36d4fe138e63d105ab2862e8c
Przemek <wisniewskip90@gmail.com>
2024-06-22 10:55:32 +0200

    add corrections in models.py

--- reports/models.py
+++ reports/models.py
@@ -3,17 +3,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Report(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'Report {self.id} by {self.user.username}'
 
 class Product(models.Model):
-    sku = models.CharField(max_length=7)
-    product_name = models.CharField(max_length=255)
-    quantity_stock = models.IntegerField()
-    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    sku = models.CharField(max_length=7, null=False, blank=False)
+    product_name = models.CharField(max_length=255, null=False, blank=False)
+    quantity_stock = models.IntegerField(null=True, blank=True)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
     entry_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -25,9 +25,9 @@ class Product(models.Model):
         super(Product, self).save(*args, **kwargs)
 
 class ReportProduct(models.Model):
-    report = models.ForeignKey(Report, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity_found = models.IntegerField()
+    report = models.ForeignKey(Report, on_delete=models.CASCADE, null=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False, blank=False)
+    quantity_found = models.IntegerField(null=False, blank=False)
 
     def __str__(self):
         return f'ReportProduct {self.id} - Report {self.report.id} - Product {self.product.id}'
