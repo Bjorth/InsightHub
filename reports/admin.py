34c612cbd8f2a9d5ee3dd2b6e03fc6f4cb7f1ade
Przemek <wisniewskip90@gmail.com>
2024-06-25 17:03:30 +0200

    add comments in admin.py

--- reports/admin.py
+++ reports/admin.py
@@ -3,10 +3,11 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from .models import Report, ReportProduct, Product
 
-# Register your models here.
+# Registered models from models.py
 admin.site.register(Report)
 admin.site.register(ReportProduct)
 admin.site.register(Product)
 
+# Default settings from Django for registration and account management
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
