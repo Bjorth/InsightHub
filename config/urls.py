3d1d4bec77fab2235188118f2516c944fbcbad92
Przemek <wisniewskip90@gmail.com>
2024-06-25 17:01:41 +0200

    Ordered url.py with comments

--- config/urls.py
+++ config/urls.py
@@ -20,23 +20,29 @@ from django.urls import path, include
 from reports import views
 
 urlpatterns = [
+    # Main path below
     path('', views.index_view, name='index'),
+    # Admin and accounts paths below
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('register/', views.register_view, name='register'),
+    # Product paths below
     path('products/', views.product_view, name='product_view'),
     path('products/create/', views.product_create, name='product_create'),
     path('products/update/<pk>', views.product_update, name='product_update'),
     path('products/delete/<pk>', views.product_delete, name='product_delete'),
+    # Product reports paths below
     path('product_reports/', views.product_report_view, name='product_report_view'),
     path('product_reports/create/', views.product_report_create, name='product_report_create'),
     path('product_reports/udpate/<pk>', views.product_report_update, name='product_report_update'),
     path('product_reports/delete/<pk>', views.product_report_delete, name='product_report_delete'),
-    path('reports/view/', views.report_view, name='report_view'),
-    path('reports/detail/<int:report_id>/', views.report_detail, name='report_detail'),
     path('product_reports/detail/<int:product_report_id>/', views.product_report_detail, name='product_report_detail'),
     path('product_reports/<int:product_report_id>/edit/', views.product_report_edit, name='product_report_edit'),
+    # Reports path below
+    path('reports/view/', views.report_view, name='report_view'),
+    path('reports/detail/<int:report_id>/', views.report_detail, name='report_detail'),
     path('reports/<int:report_id>/edit/', views.report_edit, name='report_edit'),
     path('reports/<int:report_id>/delete/', views.report_delete, name='report_delete'),
+    # Generate pdf report file path below
     path('reports/<int:report_id>/pdf/', views.generate_pdf_report, name='generate_pdf_report'),
 ]
